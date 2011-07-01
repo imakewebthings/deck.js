7c1c3f6b66e81887829bfd8e0f9bc40cd35d8338
imakewebthings <caleb@imakewebthings.com>
2011-07-01 11:21:38 +0800

    Removing some superfluous ish

--- test/spec.core.js
+++ test/spec.core.js
@@ -1,11 +1,8 @@
 // Go tests, go
 describe('Deck JS', function() {
-	zzz = 0;
-	
 	describe('standard html structure', function() {
 		beforeEach(function() {
 			loadFixtures('standard.html');
-			zzz++;
 		});
 
 		describe('init(selector)', function() {
