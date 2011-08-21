05e41e8dcc111f58365ad0fe06eb6b83970dafac
imakewebthings <caleb@imakewebthings.com>
2011-08-21 14:14:08 +0800

    Add deck.hash theming, include in intro deck

--- extensions/hash/deck.hash.js
+++ extensions/hash/deck.hash.js
@@ -6,13 +6,26 @@ https://github.com/imakewebthings/deck.js/blob/master/MIT-license.txt
 https://github.com/imakewebthings/deck.js/blob/master/GPL-license.txt
 */
 
+/*
+This module adds deep linking to individual slides and updates the
+address bar with the hash as the user moves through the deck. A permalink
+anchor is also updated. Standard themes hide this link in browsers that
+support the History API, and show it for those that do not. Slides that do
+not have an id are assigned one according to the hashPrefix option.
+*/
 (function ($, deck, window, undefined) {
 	var $d = $(document),
-	$internals,
 	$window = $(window),
 	
-	goByHash = function(hash) {
-		var id = hash.substr(hash.indexOf("#") + 1),
+	/* Collection of internal fragment links in the deck */
+	$internals,
+	
+	/*
+	Internal only function.  Given a string, extracts the id from the hash,
+	matches it to the appropriate slide, and navigates there.
+	*/
+	goByHash = function(str) {
+		var id = str.substr(str.indexOf("#") + 1),
 		slides = $[deck]('getSlides');
 		
 		$.each(slides, function(i, $el) {
@@ -23,6 +36,18 @@ https://github.com/imakewebthings/deck.js/blob/master/GPL-license.txt
 		});
 	};
 	
+	/*
+	Extends defaults/options.
+	
+	options.selectors.hashLink
+		The element matching this selector has its href attribute updated to
+		the hash of the current slide as the user navigates through the deck.
+		
+	options.hashPrefix
+		Every slide that does not have an id is assigned one at initialization.
+		Assigned ids take the form of hashPrefix + slideIndex, e.g., slide-0,
+		slide-12, etc.
+	*/
 	$.extend(true, $[deck].defaults, {
 		selectors: {
 			hashLink: '.deck-permalink'
@@ -55,11 +80,14 @@ https://github.com/imakewebthings/deck.js/blob/master/GPL-license.txt
 		});
 		
 		if (!Modernizr.hashchange) {
+			/* Set up internal links using click for the poor browsers
+			without a hashchange event. */
 			$internals.bind('click.deck', function(e) {
 				goByHash($(this).attr('href'));
 			});
 		}
 	})
+	/* Update permalink and address bar on a slide change */
 	.bind('deck.change', function(e, from, to) {
 		var hash = '#' + $[deck]('getSlide', to).attr('id');
 		
@@ -69,6 +97,7 @@ https://github.com/imakewebthings/deck.js/blob/master/GPL-license.txt
 		}
 	});
 	
+	/* Deals with internal links in modern browsers */
 	$window.bind('hashchange.deck', function(e) {
 		goByHash(e.originalEvent.newURL);
 	});
