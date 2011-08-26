88109bec4187450ef6f55f11f00a41db36873278
imakewebthings <caleb@imakewebthings.com>
2011-08-26 22:09:03 +0800

    First rough take at scaling

--- extensions/scale/deck.scale.js
+++ extensions/scale/deck.scale.js
@@ -9,30 +9,78 @@ https://github.com/imakewebthings/deck.js/blob/master/GPL-license.txt
 /*
 
 */
-(function($, deck, undefined) {
-	var $d = $(document);
+(function($, deck, window, undefined) {
+	var $d = $(document),
+	$w = $(window),
+	baseHeight,
+	didResize,
 	
+	scaleDeck = function() {
+		var obh = $[deck]('getOptions').baseHeight,
+		container = $[deck]('getContainer'),
+		height = $w.height(),
+		slides = $[deck]('getSlides'),
+		scale;
+		
+		// Use tallest slide as base height if not set manually
+		baseHeight = obh ? obh : (function() {
+			var greatest = 0;
+			
+			$.each(slides, function(i, $slide) {
+				greatest = Math.max(greatest, $slide.outerHeight());
+			});
+			
+			return greatest;
+		})();
+		
+		scale = height / baseHeight;
+		
+		if (!$[deck]('getOptions').scaleUp && scale > 1) {
+			scale = 1;
+		}
+		
+		$.each('Webkit Moz O ms Khtml'.split(' '), function(i, prefix) {
+			container.css(prefix + 'Transform', 'scale(' + scale + ')');
+		});
+	};
+
 	/*
 	Extends defaults/options.
 	
-	options.classes.menu
-		This class is added to the deck container when showing the slide menu.
 	
-	options.keys.menu
-		The numeric keycode used to toggle between showing and hiding the slide
-		menu.
 	*/
 	$.extend(true, $[deck].defaults, {
-		
+		classes: {
+			scale: 'deck-scale'
+		},
+		baseHeight: 0,
+		scaleThrottle: 500,
+		scaleUp: false
 	});
-
-	
 
 	$d.bind('deck.init', function() {
+		// Only care about scaling non-embedded presentations
+		if (!$[deck]('getContainer').is('body')) return;
 		
-	})
-	.bind('deck.change', function(e, from, to) {
+		$[deck]('getContainer').addClass($[deck]('getOptions').classes.scale);
 		
+		// Throttle scaling on resize
+		$w.unbind('resize.deckscale').bind('resize.deckscale', function() {
+			if (!didResize) {
+				didResize = true;
+				window.setTimeout(function() {
+					scaleDeck();
+					didResize = false;
+				}, $[deck]('getOptions').scaleThrottle);
+			}
+		})
+		// Scale once on load, in case images or something change layout
+		.unbind('load.deckscale').bind('load.deckscale', scaleDeck);
+		
+		// Scale once on init
+		scaleDeck();
 	});
-})(jQuery, 'deck');
+	
+	// Scale again on load, in case images or anything else changed layout
+})(jQuery, 'deck', this);
 
