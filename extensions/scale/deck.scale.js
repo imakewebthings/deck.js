88b72478e5aff72b51bd95c36f5d33d493b2c8a0
imakewebthings <caleb@imakewebthings.com>
2011-08-26 10:13:05 +0800

    Adding empty scale

---
+++ extensions/scale/deck.scale.js
@@ -0,0 +1,38 @@
+/*!
+Deck JS - deck.scale - v1.0
+Copyright (c) 2011 Caleb Troughton
+Dual licensed under the MIT license and GPL license.
+https://github.com/imakewebthings/deck.js/blob/master/MIT-license.txt
+https://github.com/imakewebthings/deck.js/blob/master/GPL-license.txt
+*/
+
+/*
+
+*/
+(function($, deck, undefined) {
+	var $d = $(document);
+	
+	/*
+	Extends defaults/options.
+	
+	options.classes.menu
+		This class is added to the deck container when showing the slide menu.
+	
+	options.keys.menu
+		The numeric keycode used to toggle between showing and hiding the slide
+		menu.
+	*/
+	$.extend(true, $[deck].defaults, {
+		
+	});
+
+	
+
+	$d.bind('deck.init', function() {
+		
+	})
+	.bind('deck.change', function(e, from, to) {
+		
+	});
+})(jQuery, 'deck');
+
