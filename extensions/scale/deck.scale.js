927f6e4b1b4fb594d1393190716cc998e456b732
Alex Chaffee <alex@stinky.com>
2012-01-16 13:26:07 -0800

    scale extension scales each slide separately

--- extensions/scale/deck.scale.js
+++ extensions/scale/deck.scale.js
@@ -22,7 +22,7 @@ works fine.
 	$w = $(window),
 	baseHeight, // Value to scale against
 	timer, // Timeout id for debouncing
-	
+
 	/*
 	Internal function to do all the dirty work of scaling the deck container.
 	*/
@@ -33,105 +33,106 @@ works fine.
 		slides = $[deck]('getSlides'),
 		scale,
 		transform;
-		
+
 		// Don't scale if scaling disabled
 		if (!$container.hasClass($[deck]('getOptions').classes.scale)) {
 			scale = 1;
 		}
 		else {
-			// Use tallest slide as base height if not set manually
+			// Use window height as base height if not set manually
 			baseHeight = obh ? obh : (function() {
-				var greatest = 0;
-
-				$.each(slides, function(i, $slide) {
-					greatest = Math.max(greatest, $slide.outerHeight());
-				});
-
-				return greatest;
+				var slop = 40; /* for padding */
+			  var windowHeight = $(window).height();
+				return windowHeight - slop;
 			})();
-			
-			scale = height / baseHeight;
 		}
-		
-		// Scale, but don't scale up
-		transform = scale >= 1 ? 'none' : 'scale(' + scale + ')';
-		$.each('Webkit Moz O ms Khtml'.split(' '), function(i, prefix) {
-			$container.css(prefix + 'Transform', transform);
-		});
-	};
+
+		// Scale each slide down if necessary (but don't scale up)
+		$.each(slides, function(i, $slide) {
+		  var slideHeight = $slide.innerHeight();
+		  var scale = baseHeight / slideHeight;
+
+		  if (scale < 1) {
+		    transform = 'scale(' + scale + ')';
+  		  $.each('Webkit Moz O ms Khtml'.split(' '), function(i, prefix) {
+  			  $slide.css(prefix + 'Transform', transform);
+  		  });
+		  }
+	  });
+  }
 
 	/*
 	Extends defaults/options.
-	
+
 	options.classes.scale
 		This class is added to the deck container when scaling is enabled.
 		It is enabled by default when the module is included.
-	
+
 	options.keys.scale
 		The numeric keycode used to toggle enabling and disabling scaling.
-	
+
 	options.baseHeight
 		When baseheight is falsy, as it is by default, the deck is scaled
 		in proportion to the height of the slides.  You may instead specify
 		a height, and the deck will be scaled against this height regardless
 		of the actual content height.
-	
+
 	options.scaleDebounce
 		Scaling on the browser resize event is debounced. This number is the
 		threshold in milliseconds. You can learn more about debouncing here:
 		http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-	
+
 	*/
 	$.extend(true, $[deck].defaults, {
 		classes: {
 			scale: 'deck-scale'
 		},
-		
+
 		keys: {
 			scale: 83 // s
 		},
-		
+
 		baseHeight: null,
 		scaleDebounce: 200
 	});
-	
+
 	/*
 	jQuery.deck('disableScale')
-	
+
 	Disables scaling and removes the scale class from the deck container.
 	*/
 	$[deck]('extend', 'disableScale', function() {
 		$[deck]('getContainer').removeClass($[deck]('getOptions').classes.scale);
 		scaleDeck();
 	});
-	
+
 	/*
 	jQuery.deck('enableScale')
-	
+
 	Enables scaling and adds the scale class to the deck container.
 	*/
 	$[deck]('extend', 'enableScale', function() {
 		$[deck]('getContainer').addClass($[deck]('getOptions').classes.scale);
 		scaleDeck();
 	});
-	
+
 	/*
 	jQuery.deck('toggleScale')
-	
+
 	Toggles between enabling and disabling scaling.
 	*/
 	$[deck]('extend', 'toggleScale', function() {
 		var $c = $[deck]('getContainer');
 		$[deck]($c.hasClass($[deck]('getOptions').classes.scale) ?
-			'disableScale' : 'enableScale'); 
+			'disableScale' : 'enableScale');
 	});
 
 	$d.bind('deck.init', function() {
 		var opts = $[deck]('getOptions');
-		
+
 		// Scaling enabled at start
 		$[deck]('getContainer').addClass(opts.classes.scale);
-		
+
 		// Debounce the resize scaling
 		$w.unbind('resize.deckscale').bind('resize.deckscale', function() {
 			window.clearTimeout(timer);
@@ -139,7 +140,7 @@ works fine.
 		})
 		// Scale once on load, in case images or something change layout
 		.unbind('load.deckscale').bind('load.deckscale', scaleDeck);
-		
+
 		// Bind key events
 		$d.unbind('keydown.deckscale').bind('keydown.deckscale', function(e) {
 			if (e.which === opts.keys.scale || $.inArray(e.which, opts.keys.scale) > -1) {
@@ -147,7 +148,7 @@ works fine.
 				e.preventDefault();
 			}
 		});
-		
+
 		// Scale once on init
 		scaleDeck();
 	});
