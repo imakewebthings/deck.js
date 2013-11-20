a74437b17d23b4ea7e7ac7279f0c78da70d3b7ff
imakewebthings <caleb@imakewebthings.com>
2013-11-19 16:15:22 -0800

    Restore theme selector functionality for getting started page

--- introduction/introduction.js
+++ introduction/introduction.js
@@ -1,4 +1,22 @@
 $(function() {
+  var $frame = $('.deck-frame');
 
+  var resetScaling = function() {
+    $frame[0].contentWindow.$.deck('toggleScale');
+    $frame[0].contentWindow.$.deck('toggleScale');
+  };
+
+  var replaceLink = function(linkSelector, linkHref) {
+    $frame.contents().find(linkSelector).attr('href', linkHref);
+    resetScaling();
+  };
+
+  $('#style-themes').change(function() {
+    replaceLink('#style-theme-link', $(this).val());
+  });
+
+  $('#transition-themes').change(function() {
+    replaceLink('#transition-theme-link', $(this).val());
+  });
 });
 
